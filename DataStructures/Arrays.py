a4b9f4c0d3fec8339d5bdb29ab2cc63543d7a042
umairmohd8 <mohammed08@protonmail.com>
2020-08-20 00:09:31 +0530

    problems

---
+++ DataStructures/Arrays.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    s = len(a)
+    newA = []
+    for i in range(s):
+        newA.append(a.pop())
+    return newA
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+# https://www.hackerrank.com/challenges/arrays-ds/problem
